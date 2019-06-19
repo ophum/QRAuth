0c36cae3ec5bbc15877f085fc358233cf7991bc8
ophum <inagaki0106@gmail.com>
2019-06-19 22:26:18 +0900

    [add] databaseを扱えるようにした

--- Route.php
+++ Route.php
@@ -7,6 +7,11 @@ $config = [
             'blade_template_path' => __DIR__ . '/views',
             'blade_cache_path' => __DIR__ . '/cache',
         ],
+		'db' => [
+            'dsn' => 'sqlite:database.sqlite',
+            'user' => '',
+            'password' => '',
+		],
     ],
 ];
 
@@ -14,11 +19,20 @@ $config = [
 $app = new \Slim\App($config);
 
 $container = $app->getContainer();
+
 $container['view'] = function($container) {
     return new \Slim\Views\Blade(
         $container['settings']['renderer']['blade_template_path'],
         $container['settings']['renderer']['blade_cache_path']
     );
+};
+
+$container['db'] = function($container) {
+	$db = $container['settings']['db'];
+	$pdo = new PDO($db['dsn'], $db['user'], $db['password']);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	return $pdo;	
 };
 
 $app->get('/', TokenController::class.":index");
