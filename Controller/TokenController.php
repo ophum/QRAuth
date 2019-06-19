09e915b3401d78430c1cab62357a7733ebf6ec65
ophum <inagaki0106@gmail.com>
2019-06-19 22:27:01 +0900

    [add] tokenを作成して登録

--- Controller/TokenController.php
+++ Controller/TokenController.php
@@ -5,16 +5,29 @@ use Slim\Http\Response;
 
 class TokenController {
     private $app;
+    private $db;
 
     public function __construct(Container $app) {
         $this->app = $app;
+        $this->db = $app['db'];
     }
 
     //
     public function index(Request $request, Response $response) {
         
-        $array = ['token' => "12345567"];
+        $array = ['token' => $this->createToken()];
 
         return $this->app->view->render($response, 'index', $array);
+    }
+
+
+    private function createToken() {
+        $tok = sha1(uniqid(rand(), true));
+
+        $res = $this->db->prepare("insert into tokens(token) values(:tok)");
+        $res->bindParam(":tok", $tok);
+        $res->execute();
+
+        return $tok;
     }
 }
